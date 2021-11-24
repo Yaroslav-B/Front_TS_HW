d5b9b1863de5744eeac9fd11e166bbd6dd0ab09e
Yaroslav Bobyk <sync2galaxy@gmail.com>
2021-11-24 10:57:03 +0200

    Changed configuration for using TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,18 @@ module.exports = {
         basket: "./modules/basket.js",
         addNewProduct: "./modules/addNewProduct.js",
     },
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+      },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
@@ -17,5 +29,5 @@ module.exports = {
             directory: path.join(__dirname, 'dist'),
           }
     },
-    // devtool: "source-map"
+    devtool: 'inline-source-map',
 }
